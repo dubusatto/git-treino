c46663744210e6d54890f0adf153a5f4ff55a0a0
dubusatto <du.busatto@gmail.com>
2026-01-21 14:15:44 -0300

    feat: minha multiplicacao local

--- calculadora.py
+++ calculadora.py
@@ -4,6 +4,9 @@ def somar(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+    
 if __name__ == "__main__":
     print("Calculadora v1.0")
     print("Soma 2 + 2 =", somar(2, 2))
